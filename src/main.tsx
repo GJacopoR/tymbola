8d615cd541d2c37b6e341d59d363232945de4e5c
Gennaro Jacopo Russo <gennarojacopo@libero.it>
2024-01-11 02:50:46 +0100

    Upgrade to React 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { store } from './store'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from './store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
