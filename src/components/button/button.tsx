a63d8060b2769139a1bb4f1929f0b029fe0686f7
Gennaro Jacopo Russo <gennarojacopo@libero.it>
2024-01-16 22:32:42 +0100

    Add disabled buttons behavior

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,14 +3,16 @@ import classes from './button.module.scss'
 interface ButtonProps {
     children?: React.ReactNode,
     className?: string,
+    disabled?: boolean,
     label?: string,
     onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void,
     style?: React.CSSProperties,
 }
 
-function Button({children, className, label, onClick, style}:ButtonProps){
+function Button({children, className, disabled, label, onClick, style}:ButtonProps):JSX.Element{
     return <button
         className={className ? classes.defaultButton + ' ' + className : classes.defaultButton}
+        disabled={disabled}
         onClick={onClick}
         style={style}>
             {children} {label}
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -17,7 +17,7 @@ function Home({onModalOpen}:HomeComponentProps) {
         </header>
         <main className={classes.buttons}>
             <Link to={'/tymbola/caller'} className={classes.link}>
-                <Button className={classes.button} label={"Assuntore \n (chi chiama)"}/>
+                <Button className={classes.button} label={"Tenitore \n (chi chiama)"}/>
             </Link>
 
             <Button className={classes.button} label={"Giocatore \n (chi spunta)"} onClick={onModalOpen} />
