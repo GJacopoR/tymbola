1b5f0658f5e75541388fd641781349ba7d0636fc
Gennaro Jacopo Russo <gennarojacopo@libero.it>
2024-02-10 00:38:35 +0100

    [WIP] Add background image

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -4,6 +4,7 @@ import CardSelectorModal from "../../components/card-selector-modal";
 import Button from "../../components/button";
 import Header from "../../components/header";
 import PageTransition from "../../assets/page-transition";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface HomeComponentProps {
   isCallerGameOngoing: boolean;
@@ -49,57 +50,65 @@ function Home({
             </section>
           </section>
 
-          {(isCallerGameOngoing || isPlayerGameOngoing) &&
-            !window.location.pathname.includes("player") && (
-              <section className={classes.ongoingGameSection}>
-                <header>
-                  <svg
-                    fill="#ff0000"
-                    viewBox="0 0 16 16"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                    <g
-                      id="SVGRepo_tracerCarrier"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></g>
-                    <g id="SVGRepo_iconCarrier">
-                      <path
-                        d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm0-2A6 6 0 1 1 8 2a6 6 0 0 1 0 12zM7 9h2V4H7v5zm0 3h2v-2H7v2z"
-                        fillRule="evenodd"
-                      ></path>
-                    </g>
-                  </svg>
+          <AnimatePresence>
+            {(isCallerGameOngoing || isPlayerGameOngoing) &&
+              !window.location.pathname.includes("player") && (
+                <motion.section
+                  className={classes.ongoingGameSection}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <header>
+                    <svg
+                      fill="#ff0000"
+                      viewBox="0 0 16 16"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        <path
+                          d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm0-2A6 6 0 1 1 8 2a6 6 0 0 1 0 12zM7 9h2V4H7v5zm0 3h2v-2H7v2z"
+                          fillRule="evenodd"
+                        ></path>
+                      </g>
+                    </svg>
 
-                  <h4 className={classes.alertTitle}>
-                    {`C'è una partita ${
-                      isCallerGameOngoing ? "tenitore" : "giocatore"
-                    } attualmente in corso.`}
-                  </h4>
+                    <h4 className={classes.alertTitle}>
+                      {`C'è una partita ${
+                        isCallerGameOngoing ? "tenitore" : "giocatore"
+                      } attualmente in corso.`}
+                    </h4>
 
-                  {!isCallerGameOngoing && (
-                    <p className={classes.alertSubTitle}>
-                      Terminandola, eventuali cartelle non salvate andranno
-                      perse.
-                    </p>
-                  )}
-                </header>
+                    {!isCallerGameOngoing && (
+                      <p className={classes.alertSubTitle}>
+                        Terminandola, eventuali cartelle non salvate andranno
+                        perse.
+                      </p>
+                    )}
+                  </header>
 
-                <Button
-                  className={classes.button}
-                  label={"Termina partita"}
-                  onClick={
-                    isCallerGameOngoing ? onEndCallerGame : onEndPlayerGame
-                  }
-                />
-                <Button
-                  className={classes.button}
-                  label={"Vai alla partita"}
-                  onClick={onGameReturn}
-                />
-              </section>
-            )}
+                  <Button
+                    className={classes.button}
+                    label={"Termina partita"}
+                    onClick={
+                      isCallerGameOngoing ? onEndCallerGame : onEndPlayerGame
+                    }
+                  />
+                  <Button
+                    className={classes.button}
+                    label={"Vai alla partita"}
+                    onClick={onGameReturn}
+                  />
+                </motion.section>
+              )}
+          </AnimatePresence>
         </main>
         <aside>
           <CardSelectorModal />
